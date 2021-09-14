96ba8784e3452e336e9738a16ea0855780c57751
Naf-Alpha <muhammad2233ft.2021@student.uny.ac.id>
2021-09-14 14:55:59 +0700

    Tugas 1

---
+++ Pertemuan 3/Tugas1.java
@@ -0,0 +1,19 @@
+// Identitas Diri
+public class Tugas1 {
+    public static void main(String[] args) {
+        String nama = "Muhammad Naufal Alfawwaz";
+        String nim = "21537144024";
+        int ang = 2021;
+        String prodi = "Teknologi Informasi";
+        String jurusan = "JPTEI (Jurusan Pendidikan Teknik Elektronika dan Informatika)";
+        String fakultas = "Fakultas Teknik";
+        double ipk = 4.0;
+        System.out.println("Nama = "+nama);
+        System.out.println("NIM = "+nim);
+        System.out.println("Angkatan = "+ang);
+        System.out.println("Program Studi = "+prodi);
+        System.out.println("Jurusan = "+jurusan);
+        System.out.println("Fakultas = "+fakultas);
+        System.out.println("IPK = "+ipk);
+    }
+}
---
+++ Pertemuan 3/tugas1.py
@@ -0,0 +1,15 @@
+"""Identitas Diri"""
+nama = "Muhammad Naufal Alfawwaz"
+nim = 21537144024
+ang = 2021
+prodi = "Teknologi Informasi"
+jurusan = "JPTEI (Jurusan Pendidikan Teknik Elektronika dan Informatika)"
+fakultas = "Fakultas Teknik"
+ipk = 4.0
+print("Nama = "+nama)
+print("NIM = "+str(nim))
+print("Angkatan = "+str(ang))
+print("Program Studi = "+prodi)
+print("Jurusan = "+jurusan)
+print("Fakultas = "+fakultas)
+print("IPK = "+str(ipk))
