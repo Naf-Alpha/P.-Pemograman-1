e5fe373ac20a2cd80d300a6661edd1ec7def4f8d
Naf-Alpha <muhammad2233ft.2021@student.uny.ac.id>
2021-09-14 14:26:05 +0700

    No Update

--- Pertemuan 3/Saya.java
+++
@@ -1,9 +0,0 @@
-/*
-Nama : Muhammad Naufal Alfawwaz
-Kelas : J2
-*/
-public class Saya {
-    public static void main(String[] args) {
-        System.out.println("Muhammad Naufal Alfawwaz");
-    }
-}
--- Pertemuan 3/nama.py
+++
@@ -1,9 +0,0 @@
-Nama = "Muhammad Naufal Alfawwaz"
-Umur = 18
-BB = 70.5
-print("Nama saya "+Nama)
-print("Umur saya "+str(Umur)+" Tahun")
-print("Berat badan saya "+str(BB)+" kg")
-print(type(Nama))
-print(type(Umur))
-print(type(BB))
